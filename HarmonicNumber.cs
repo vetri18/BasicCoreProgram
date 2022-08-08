3a1d552dceaa0f4e23d2ac2d01c956723aa3cdfa
vetrivel <vetrivel.kce@gmail.com>
2022-08-08 14:49:28 +0530

    question four

---
+++ HarmonicNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class HarmonicNumber
+    {
+        public static void HarmonicNum()
+        {
+            int i, n;
+            double s = 0.0;
+            Console.Write("\n\n");
+
+            Console.Write("Input the number of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("1/{0} + ", i);
+                s += 1 / (float)i;
+            }
+            Console.Write("\nSum of Series upto {0} terms : {1} \n", n, s);
+        }
+    }
+}
