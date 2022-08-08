0941353487ab77e929a40cf605ec23053d64a059
vetrivel <vetrivel.kce@gmail.com>
2022-08-08 14:38:56 +0530

    welcome to basic core programs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace BasicCorePrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Basic Core Programs");
+        }
+    }
+}
