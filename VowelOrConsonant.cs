8b710e6199e182d1fc0314218f0371b45ab5aa84
vetrivel <vetrivel.kce@gmail.com>
2022-08-08 15:01:53 +0530

    question nine

---
+++ VowelOrConsonant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class VowelOrConsonant
+    {
+        public static void VowelConsonant()
+        {
+            Console.WriteLine("Type Charater");
+            char z = Convert.ToChar(Console.ReadLine());
+            if (z == 'a' || z == 'e' || z == 'i' || z == 'o' || z == 'u' || z == 'A' || z == 'E' || z== 'I' || z == 'O' || z == 'U')
+            {
+                Console.WriteLine("Vowel");
+            }
+            else
+            {
+                Console.WriteLine("Consonatnt");
+            }
+        }
+    }
+}
