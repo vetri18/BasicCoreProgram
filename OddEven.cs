5a344d83c5d761aff2f6f457087a509c94dd55b7
vetrivel <vetrivel.kce@gmail.com>
2022-08-08 14:59:50 +0530

    question eight

---
+++ OddEven.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class OddEven
+    {
+        public static void OddEvenValue()
+        {
+            Console.WriteLine("Number ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+    }
+}
